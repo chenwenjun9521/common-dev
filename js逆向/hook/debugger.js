207686e8dc90a16088c65adac8bdaeedf5d4d903
chenwenjun9521 <wenjun.chen@v-trace.cn>
2025-09-09 18:43:16 +0800

    更改

---
+++ js逆向/hook/debugger.js
@@ -0,0 +1,8 @@
+var _constructor = Function.prototype.constructor;
+Function.prototype.constructor = function (s) {
+    if (s === 'debugger') {
+        console.log(s);
+        return null;
+    }
+    return _constructor(s);
+}
